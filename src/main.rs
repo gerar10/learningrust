75d8481eac263116efc4d7c1e917275368318743
gerar10 <gerarburgos1987@gmail.com>
2023-03-12 18:47:38 -0300

    trabajos con if/else

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,48 @@
 #[derive(PartialEq, Debug)]
-struct Car { 
-    color: String, 
-    motor: Transmission, 
-    roof: bool, 
-    age: (Age, u32) }
+struct Car { color: String, motor: Transmission, roof: bool, age: (Age, u32) }
 
 #[derive(PartialEq, Debug)]
-enum Transmission { 
-    Manual, 
-    SemiAuto, 
-    Automatic }
+enum Transmission { Manual, SemiAuto, Automatic }
 
 #[derive(PartialEq, Debug)]
-enum Age { 
-    New, 
-    Used }
+enum Age { New, Used }
 
-    fn car_quality (miles: u32) -> (Age, u32) {
-        let quality: (Age, u32) = (Age::New, miles);
-        quality
+fn car_quality (miles: u32) -> (Age, u32) {
+    if miles > 0 {
+        return (Age::Used, miles)
     }
-
-
-fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
-    Car {
-        color: color,
-        motor: motor,
-        roof: roof,
-        age: car_quality(miles), 
-    }
+    (Age::New, miles)
 }
 
+fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
+  if car_quality(miles).0 == Age::Used {
+      if roof {
+          println!("Preparing a used car: {:?}, {}, Hard top, {} miles ", motor, color, miles);
+      } else {
+          println!("Preparing a used car: {:?}, {}, Convertible, {} miles", motor, color, miles);
+      } 
+      }
+      else {
+          if roof {
+              println!("Building a new car: {:?}, {}, Hard top, {} miles", motor, color, miles);
+          } else {
+              println!("biulding a new car: {:?}, {}, Convertible, {} miles", motor, color, miles)
+          }
+      }
+    
+
+  Car {
+      color: color,
+      motor: motor,
+      roof: roof,
+      age: car_quality(miles)
+  }
+}
 
 fn main() {
-    let colors = ["blue", "Green", "Red", "Silver"];
-    let mut car: Car;     
-    let mut engine = Transmission::Manual;
+  car_factory(String::from("Orange"), Transmission::Manual, true, 0);
 
-    car = car_factory(String::from(colors[0]), engine, true, 0);
-    println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+  car_factory(String::from("Red"), Transmission::SemiAuto, false, 565);
 
-
-    engine = Transmission::SemiAuto;
-    car = car_factory(String::from(colors[1]), engine, false, 100);
-    println!("Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
-
-    // Car order #3: Used, Automatic, Hard top
-    engine = Transmission::Automatic;
-    car = car_factory(String::from(colors[2]), engine, true, 200);
-    println!("Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+  car_factory(String::from("White"), Transmission::Automatic, true, 3000);
 }
