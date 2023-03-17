175643bf9e11486ab092578a711bb740c96a4743
gerar10 <gerarburgos1987@gmail.com>
2023-03-17 09:07:30 -0300

    split code into modules

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
-struct Container<T> {
-    value: T,
-}
+mod authentication {
+    use std::collections::hash_map::DefaultHasher;
+    use std::hash::{Hash, Hasher};
 
-impl<T> Container<T> {
-    pub fn new(value: T) -> Self {
-        Container { value }
+    pub struct User {
+        username: String,
+        password_hash: u64,
+    }
+
+    impl User {
+        pub fn new(username: &str, password: &str) -> User {
+            User {
+                username: username.to_string(),
+                password_hash: hash_password(&password.to_owned()),
+            }
+        }
+
+        pub fn get_username(&self) -> &String {
+            &self.username
+        }
+
+        pub fn set_password(&mut self, new_password: &str) {
+            self.password_hash = hash_password(&new_password.to_owned())
+        }
+    }
+    fn hash_password<T: Hash>(t: &T) -> u64 {
+        let mut s = DefaultHasher::new();
+        t.hash(&mut s);
+        s.finish()
     }
 }
 
 fn main() {
-    assert_eq!(Container::new(42).value, 42);
-    assert_eq!(Container::new(3.14).value, 3.14);
-    assert_eq!(Container::new("Foo").value, "Foo");
-    assert_eq!(Container::new(String::from("Bar")).value, String::from("Bar"));
-    assert_eq!(Container::new(true).value, true);
-    assert_eq!(Container::new(-12).value, -12);
-    assert_eq!(Container::new(Some("text")).value, Some("text"));
+    let mut user = authentication::User::new("jeremy", "super-secret");
+
+    println!("The username is: {}", user.get_username());
+    user.set_password("even-more-secret");
 }
